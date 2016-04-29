1785cea8c614d749d90fb7e8919c25bd37d200d3
Morel Sébastien <morel.seb@gmail.com>
2016-04-28 22:57:11 -0700

    First Stable

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'stats_api'          => 'Server',
+    'slabs_api'          => 'Server',
+    'items_api'          => 'Server',
+    'get_api'            => 'Server',
+    'set_api'            => 'Server',
+    'delete_api'         => 'Server',
+    'flush_all_api'      => 'Server',
+    'connection_timeout' => '1',
+    'max_item_dump'      => '100',
+    'refresh_rate'       => 5,
+    'memory_alert'       => '80',
+    'hit_rate_alert'     => '90',
+    'eviction_alert'     => '0',
+    'file_path'          => '/tmp',
+    'servers'            =>
+        [
+            'Default' =>
+                [
+                    'Memcache' =>
+                        [
+                            'hostname' => getenv('MEMCACHE_HOST'),
+                            'port'     => getenv('MEMCACHE_PORT'),
+                        ],
+                ],
+        ],
+];
